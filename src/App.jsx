bfc33dd21ef242dbc2ab8425e59ca771e0929efc
1343873382 <1343873382@qq.com>
2021-03-28 23:32:40 +0800

    路由结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 import "./app.scss"
+import {BrowserRouter,Route,Switch} from "react-router-dom"
+import Home from "./pages/home/home"
+import Guide from "./pages/guide/guide"
+import Hall  from "./pages/hall/hall"
+import Information  from "./pages/information/information"
+import Question  from "./pages/question/question"
+import Result  from "./pages/result/result"
+import Rule from "./pages/rule/rule"
+import Show from './pages/show/show'
 export default class App extends Component {
   render() {
     return (
-      <div >
-        app
-      </div>
+      <BrowserRouter>
+      {/* 只匹配其中一个 */}
+      <Switch>  
+      <Route path="/home" component={Home}></Route>
+      <Route path="/guide" component={Guide}></Route>
+      <Route path="/hall" component={Hall}></Route>
+      <Route path="/information" component={Information}></Route>
+      <Route path="/question" component={Question}></Route>
+      <Route path="/result" component={Result}></Route>
+      <Route path="/rule" component={Rule}></Route>
+      <Route path="/show" component={Show}></Route>
+      </Switch>
+    
+      </BrowserRouter>
     )
   }
 }
---
+++ src/pages/hall/hall.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Hall extends Component {
+    render() {
+        return (
+            <div>
+                Hall
+            </div>
+        )
+    }
+}
