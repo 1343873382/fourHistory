8315522ccfee6b2f0a390a8b5252293966f6e6d3
1343873382 <1343873382@qq.com>
2021-03-30 11:27:06 +0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import "./app.scss"
-import {BrowserRouter,Route,Switch} from "react-router-dom"
+import {BrowserRouter,Route,Switch,Redirect} from "react-router-dom"
 import Home from "./pages/home/home"
 import Guide from "./pages/guide/guide"
 import Hall  from "./pages/hall/hall"
@@ -15,7 +15,8 @@ export default class App extends Component {
       <BrowserRouter>
       {/* 只匹配其中一个 */}
       <Switch>  
-      <Route path="/home" component={Home}></Route>
+      {/* <Redirect from='/' exact to='/home'/> */}
+      <Route exact path="/" component={Home}></Route>
       <Route path="/guide" component={Guide}></Route>
       <Route path="/hall" component={Hall}></Route>
       <Route path="/information" component={Information}></Route>
@@ -23,6 +24,7 @@ export default class App extends Component {
       <Route path="/result" component={Result}></Route>
       <Route path="/rule" component={Rule}></Route>
       <Route path="/show" component={Show}></Route>
+      
       </Switch>
     
       </BrowserRouter>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-
+import "./home.scss"
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="home">
                 Home
             </div>
         )
