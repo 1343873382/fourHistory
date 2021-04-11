1ae89f02fa5a2d02a2c336dde9027aeee0f868a0
1343873382 <1343873382@qq.com>
2021-04-11 15:54:34 +0800

    一些细节

--- src/pages/guide/guide.jsx
+++ src/pages/guide/guide.jsx
@@ -19,14 +19,41 @@ export default class Guide extends Component {
        trunBack=()=>{
            this.props.history.goBack()
        }
+       changeStyle=(index)=>{
+        let heads=document.querySelectorAll(".head")
+        for(let i=0;i<3;i++){
+         heads[i].style="none"
+         heads[i].style.transition="0.4s all"
+     }
+        heads[index].style.height="50px";
+        heads[index].style.zIndex=1;
+        heads[index].style.fontSize="18px";
+        heads[index].style.WebkitTransform="translateY(-10px)"
+        heads[index].style.transition="0.4s all"
+        heads[index].style.paddingTop="10px";
+        heads[index].style.backgroundColor="#e4c388"
+       }
+       turnPeople=()=>{  
+           let index=0;
+           this.changeStyle(index)
+
+       }
+       turnThing=()=>{
+        let index=1;
+           this.changeStyle(index)
+       }
+       turnMeeting=()=>{
+        let index=2;
+           this.changeStyle(index)
+       }
     render() {
         return (
             <div className="guide">
                 <div className="guide-title">展物指南</div>
                 <div className="content-header">
-                         <div className="head">人物区({this.state.people.num}/10)</div>
-                         <div className="head">事件区({this.state.people.num}/10)</div>
-                         <div className="head">会议区({this.state.people.num}/10)</div>
+                         <div className="head" onClick={this.turnPeople}>人物区({this.state.people.num}/10)</div>
+                         <div className="head" onClick={this.turnThing}>事件区({this.state.people.num}/10)</div>
+                         <div className="head" onClick={this.turnMeeting}>会议区({this.state.people.num}/10)</div>
                    </div>
                <div className="guide-content">
                    <div className="guide-title">
--- src/pages/question/question.jsx
+++ src/pages/question/question.jsx
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 import "./question.scss"
 import {Link} from "react-router-dom"
 export default class Question extends Component {
+    turnNo=()=>{
+        let no=document.querySelector(".question_no")
+        no.style.opacity=0.7
+        no.style.WebkitTransform="scale(0.9)"
+        no.style.transition="0.4s all"
+        setTimeout(()=>{
+          no.style="none"
+          no.style.transition="0.4s all"
+        },400)
+    }
+    turnYes=()=>{
+        let yes=document.querySelector(".question_yes")
+        yes.style.opacity=0.7
+        yes.style.WebkitTransform="scale(0.9)"
+        yes.style.transition="0.4s all"
+        setTimeout(()=>{
+          yes.style="none"
+          yes.style.transition="0.4s all"
+        },400)
+    }
     render() {
         return (
             <div className="question">
@@ -15,8 +35,8 @@ export default class Question extends Component {
                     这位革命先辈是 否参加过十五届、 九中全会？
                     </div>
                     <div className="question_choose">
-                        <div className="question_yes"></div>
-                        <div className="question_no"></div>
+                        <div className="question_yes" onClick={this.turnYes}></div>
+                        <div className="question_no" onClick={this.turnNo}></div>
                     </div>
             </div>
         )
