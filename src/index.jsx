db600cba8135e614e3b698950a9f3d036036956b
kczssd <511124893@qq.com>
2021-04-26 00:35:30 +0800

    feat:html to jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
+function getToken() {
+  const token = document.location.href.split("headimgurl=")[1];
+  if (token && token.length) {
+    localStorage.setItem("openid_token2", token);
+    const openid = token.split("openid=")[1].split("&oauthstatus")[0];
+    if (!openid) {
+      localStorage.removeItem("openid_token2");
+      window.location.href =
+        "https://be-prod.redrock.team/magicloop/rushb?b=https://fe-prod.redrock.team/four-history&scope=oldphp";
+    }
+    localStorage.setItem("openid", openid);
+  }
+  if (!localStorage.getItem("openid_token2")) {
+    window.location.href =
+      "https://be-prod.redrock.team/magicloop/rushb?b=https://fe-prod.redrock.team/four-history&scope=oldphp";
+  }
+  let Openid_token2 = localStorage.getItem("openid_token2")
+  let Token = decodeURIComponent(Openid_token2)
+  let openid = Token.split("openid=")[1].split("&oauthstatus")[0]
+  localStorage.setItem("openid", openid)
+}
+getToken();
 ReactDOM.render(
- <App />,
+  <App />,
   document.getElementById('root')
 );
