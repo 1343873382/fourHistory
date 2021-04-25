e65d9e284fa08d4b963cdcca8660779dbed7372e
yuboyuan <1343873382@qq.com>
2021-04-25 15:34:37 +0800

    拿到openid

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,4 @@ const Base = "https://be-prod.redrock.team/four-history";
 // const Base = ""
 export let setSchool = (college) => ajax(Base + "/college", { college }, "GET")
 export let setInformation = (names, collage, telephone, oppenid) => ajax(Base + "/information/create", { names, collage, telephone, oppenid }, "POST")
+export let answerQuestion = (problem_id) => ajax(Base + "problem/query", { problem_id }, "GET")
