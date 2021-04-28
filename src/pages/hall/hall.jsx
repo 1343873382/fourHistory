90bb4fa201cd3d3be261d206e6291e119efae465
yuboyuan <1343873382@qq.com>
2021-04-28 22:03:10 +0800

    问题选择最后页面

--- src/pages/hall/hall.jsx
+++ src/pages/hall/hall.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import "./hall.scss"
 import {Link} from "react-router-dom"
 export default class Hall extends Component {
+    setPeopleid=()=>{
+        sessionStorage.setItem("id",1)
+    }
+    setMeetid=()=>{
+        sessionStorage.setItem("id",58)
+    }
+    setThingid=()=>{
+        sessionStorage.setItem("id",77)
+    }
     render() {
         return (
             <div className="hall">
@@ -12,9 +21,9 @@ export default class Hall extends Component {
             <div className="hall-guide"></div>
             </Link>
               <div className="hall-choose">
-                  <Link to="/question"><div className="hall-radius">人物区</div></Link>
-                  <Link to="/question"><div className="hall-radius">会议区</div></Link>
-                  <Link to="/question"><div className="hall-radius">事件区</div></Link>
+                  <Link to="/question"><div className="hall-radius" onClick={this.setPeopleid}>人物区</div></Link>
+                  <Link to="/question"><div className="hall-radius" onClick={this.setMeetid}>会议区</div></Link>
+                  <Link to="/question"><div className="hall-radius" onClick={this.setThingid}>事件区</div></Link>
               </div>
             </div>
         )
