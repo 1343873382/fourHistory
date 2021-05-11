0cdcb6787a398c73e331f9b038e463dca2f52152
yuboyuan <1343873382@qq.com>
2021-05-11 22:02:57 +0800

    school

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const Base = "https://be-prod.redrock.team/four-history";
 
 export let setSchool = (college) => ajax(Base + "/college", { college }, "GET")
 export let setInformation = (name, school, telephone, openid) => ajax(Base + "/information/create", { name, school, telephone, openid }, "POST")
-export let updateInformation = (name, college, telephone, openid) => ajax(Base + "/information/update", { name, college, telephone, openid }, "POST")
+export let updateInformation = (name, school, telephone, openid) => ajax(Base + "/information/update", { name, school, telephone, openid }, "POST")
 export let answerQuestion = (problem_id) => ajax(Base + "problem/query", { problem_id }, "GET")
 export let showArea = (openid, area) => ajax(Base + "/guide", { openid, area }, "GET")
 export let setHall = (openid, memorial_hall_id) => ajax(Base + "/processUpdate", { openid, memorial_hall_id }, "POST")
