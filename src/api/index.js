a171128ac3dfe23a98ee4c480d02b1f28eadf9b2
yuboyuan <1343873382@qq.com>
2021-05-11 16:53:36 +0800

    学院上传

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,9 @@ const Base = "https://be-prod.redrock.team/four-history";
 // const Base = ""
 
 export let setSchool = (college) => ajax(Base + "/college", { college }, "GET")
-export let setInformation = (name, college, telephone, openid) => ajax(Base + "/information/create", { name, college, telephone, openid }, "POST")
+export let setInformation = (name, school, telephone, openid) => ajax(Base + "/information/create", { name, school, telephone, openid }, "POST")
 export let updateInformation = (name, college, telephone, openid) => ajax(Base + "/information/update", { name, college, telephone, openid }, "POST")
 export let answerQuestion = (problem_id) => ajax(Base + "problem/query", { problem_id }, "GET")
 export let showArea = (openid, area) => ajax(Base + "/guide", { openid, area }, "GET")
 export let setHall = (openid, memorial_hall_id) => ajax(Base + "/processUpdate", { openid, memorial_hall_id }, "POST")
+export let setXueyuan = (school) => ajax(Base + "/school", { school }, "GET")
--- src/pages/information/information.jsx
+++ src/pages/information/information.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import "./information.scss"
 // import { setSchool } from "../../api/index"
+import { setXueyuan } from "../../api/index"
 import { setInformation,updateInformation } from "../../api/index"
 export default class Information extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: ['通信与信息工程学院','计算机科学与技术学院','自动化学院','先进制造工程学院','光电工程学院','软件工程学院','生物信息学院','理学院','经济管理学院','传媒艺术学院','外国语学院','国际学院','网络空间安全与信息法学院','马克思主义学院','体育学院','创新创业教育学院','国防信息工程技术研究院','电子信息与网络工程研究院'],
+            data:[],
+            // data: ['通信与信息工程学院','计算机科学与技术学院','自动化学院','先进制造工程学院','光电工程学院','软件工程学院','生物信息学院','理学院','经济管理学院','传媒艺术学院','外国语学院','国际学院','网络空间安全与信息法学院','马克思主义学院','体育学院','创新创业教育学院','国防信息工程技术研究院','电子信息与网络工程研究院'],
             timeID: -1,
             height:1334
         }
@@ -76,7 +78,9 @@ export default class Information extends Component {
                 tel.value !== "请输入正确的电话号码！"&&
                 school.value !== "请输入正确的学院名称！"
             ){
+                 console.log(school.value);
                 let res = await updateInformation(name.value, school.value, tel.value, openid)
+                 console.log(res);
                 let info = res.info
                 localStorage.setItem("info", info)
                 if (res.code === 10000) {
@@ -128,12 +132,27 @@ export default class Information extends Component {
             school.style.color = "#7B4733"
             school.value = "";
         }
-        console.log(school.value.length);
-        if(school.value.length>2){
+        clearTimeout(this.state.timeID)
+        this.state.timeID = setTimeout(async () => {
+            let { data } = await setXueyuan(school.value)
+           console.log(data);
+            this.setState({
+                data:data.schools
+            })
+          if(this.state.data===null){
             select.style.display = "none"
-        }else{
+          }else{
             select.style.display = "block"
-        }
+          }
+           
+        }, 300)
+          console.log(this.state)
+        // console.log(school.value.length);
+        // if(school.value.length>2){
+        //     select.style.display = "none"
+        // }else{
+        //     select.style.display = "block"
+        // }
          
         
         // clearTimeout(this.state.timeID)
@@ -160,9 +179,9 @@ export default class Information extends Component {
         console.log(e.target.innerText);
         school.value = e.target.innerText
         select.style.display = "none"
-        // this.setState({
-        //     data: []
-        // })
+        this.setState({
+            data: []
+        })
 
     }
     render() {
